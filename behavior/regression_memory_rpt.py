0fd7290cf1905cb559a0e62217a038b45e03fd5c
Malte Rudo Güth <maltegueth@gmail.com>
2026-04-07 13:32:53 -0500

    Update regression_memory_rpt.py

--- behavior/regression_memory_rpt.py
+++ behavior/regression_memory_rpt.py
@@ -3,14 +3,14 @@ import pandas as pd
 import statsmodels.formula.api as smf
 from scipy import stats
 
+# Set the file directory and load the behavioral data
 path = 'file_directory/'
-dprime = pd.read_csv(path + 'behavior/sdt_results.csv')
+memory_data = pd.read_csv(path + 'behavior/sdt_results.csv')
 
-powtype = 'total_encoding', 'total_recall'
-memory_measure = 'd_all', 'beta_all'
+# Average data for each subject into a data frame for the regression
+regData = memory_data.groupby(['subject'], as_index=False).mean()
 
-regData = dprime.groupby(['subject'], as_index=False).mean()
-
+# Load the encoding and recall RPT data (peak power 100 ms to 600 ms after the cue
 encoding_rpt = pd.read_csv(path + 'tf/encoding_hitmiss_total_long_100600_max.csv')
 encoding_rpt = encoding_rpt[(encoding_rpt['channel'].isin(chans)) & (encoding_rpt['frequency'] == band) & (encoding_rpt['subject'].isin(subs))]
 encoding_rpt = encoding_rpt.groupby(['subject', 'reactionType'], as_index=False).mean()[['subject', 'reactionType', measure]]
@@ -19,30 +19,29 @@ recall_rpt = pd.read_csv(path + 'tf/recall_total_long_100600_max.csv')
 recall_rpt = recall_rpt[recall_rpt['channel'].isin(chans) & (recall_rpt['frequency'] == band) & (recall_rpt['subject'].isin(subs))]
 recall_rpt = recall_rpt.groupby(['subject', 'reactionType'], as_index=False).mean()[['subject', 'reactionType', measure]]
 
-regData['total_encoding'] = data1.groupby(['subject'], as_index=False).mean()['peak'].reset_index(drop=True)
-regData['total_recall'] = data2.groupby(['subject'], as_index=False).mean()['peak'].reset_index(drop=True)
+# Average the data for each subject and add it to the regression data frame
+regData['total_encoding'] = encoding_rpt.groupby(['subject'], as_index=False).mean()['peak'].reset_index(drop=True)
+regData['total_recall'] = recall_rpt.groupby(['subject'], as_index=False).mean()['peak'].reset_index(drop=True)
 
+# Z-score the data for simpler interpretation
 regData['d_all'] = stats.zscore(regData['d_all'])
 regData['beta_all'] = stats.zscore(regData['beta_all'])
-
 regData['total_encoding'] = stats.zscore(regData['total_encoding'])
 regData['total_recall'] = stats.zscore(regData['total_recall'])
 
+# Run a general linear model for each association of interest
 md = smf.glm("d_all ~ total_encoding", regData)
 mdf = md.fit()
 print(mdf.summary())
 
 md = smf.glm("beta_all ~ total_encoding", regData)
-
 mdf = md.fit()
 print(mdf.summary())
 
 md = smf.glm("d_all ~ total_recall", regData)
-
 mdf = md.fit()
 print(mdf.summary())
 
 md = smf.glm("beta_all ~ total_recall", regData)
-
 mdf = md.fit()
 print(mdf.summary())
